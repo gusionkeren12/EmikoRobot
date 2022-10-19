b9d8e8d2a7676947b4eb8e7e2cc93eb900b04b43
gusionkeren12 <116229127+gusionkeren12@users.noreply.github.com>
2022-10-20 05:41:09 +0700

    Update afk.py

--- EmikoRobot/modules/afk.py
+++ EmikoRobot/modules/afk.py
@@ -65,14 +65,14 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                "{} is here!",
-                "{} is back!",
-                "{} is now in the chat!",
-                "{} is awake!",
-                "{} is back online!",
-                "{} is finally here!",
-                "Welcome back! {}",
-                "Where is {}?\nIn the chat!",
+                "{} yah jamet balik!",
+                "{} lu ngapain kesini lagi?",
+                "{} cie korban ghosting nongol",
+                "{} hai babunya briel",
+                "{} keren lu begitu bolak balik mulu",
+                "{} pergi sana lu ga diajak!",
+                "selamat datang babu! {}",
+                "lu darimana {}?\dicariin samsudin tuh!",
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
